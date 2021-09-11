10d014efafbf895882f9fd0cd03a66df98e122bd
lapeol <86944161+cchanmi@users.noreply.github.com>
2021-09-11 13:37:17 +0900

    Update 2750(selection sort).py

--- sort/2750(selection sort).py
+++ sort/2750(selection sort).py
@@ -6,8 +6,8 @@ for _ in range(n):
 
 // 선택정렬
 
-for i in range(n):
-    min_index = i
+for i in range(n):  # 배열의 수만큼 반복
+    min_index = i   # 최소값을 첫번째 값으로 설정
     for j in range(i + 1, n):
         if array[min_index] > array[j]:  # 가장 작은 수의 인덱스를 찾기
             min_index = j
