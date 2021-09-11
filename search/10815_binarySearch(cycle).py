0c721ae777b04230bb24e40c7da15df2fe7b30dc
lapeol <86944161+cchanmi@users.noreply.github.com>
2021-09-11 19:14:53 +0900

    Update 10815_binarySearch(cycle).py

--- search/10815_binarySearch(cycle).py
+++ search/10815_binarySearch(cycle).py
@@ -9,15 +9,15 @@ card.sort()
 
 def binary_search(target):
     low, high = 0, len(card)-1
-    while low <= high:
+    while low <= high:              # low값이 high 값보다 크거나 같을 때까지 반복
         middle = (low + high) // 2
         if target == card[middle]:
             return 1
-        elif target < card[middle]:
-            high = middle -1
+        elif target < card[middle]: # 타겟값이 중간값보다 작을 경우
+            high = middle -1        # high의 값이 중간값 -1로 바뀜
         elif target > card[middle]:
             low = middle + 1
-    return 0
-
+    return 0                        # 값이 없을 경우 0 리턴
+$
 for i in card2:
     print(binary_search(i), end=' ')
