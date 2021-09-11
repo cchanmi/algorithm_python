33a71f5ff387f94572857cb9b3f49586fbcb1054
lapeol <86944161+cchanmi@users.noreply.github.com>
2021-09-11 15:12:47 +0900

    Update 2750(bubble sort).py

--- sort/2750(bubble sort).py
+++ sort/2750(bubble sort).py
@@ -5,9 +5,9 @@ for _ in range(n):
     array.append(int(input()))
 
 for i in range(len(array) -1, 0, -1):
-    for j in range(i):
-        if array[j] > array[j+1]:
-            array[j+1], array[j] = array[j], array[j+1]
+    for j in range(i):              # 0부터 n-1까지
+        if array[j] > array[j+1]:   # i번째 원소가 i+1보다 클 때
+            array[j+1], array[j] = array[j], array[j+1]   # 값을 교환해 준다
 
 for i in array:
     print(i)
